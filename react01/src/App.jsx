83d9fb2ea840e1765d14f4e2975f59f101efae72
GaganSinghal1234 <gagansinghal1806@gmail.com>
2024-12-26 15:03:49 +0530

    first

---
+++ react01/src/App.jsx
@@ -0,0 +1,15 @@
+import OOM from "./oom"
+
+function App() {
+
+
+  return (
+    <> <h1>hello world</h1>
+    <OOM/>
+    
+    </>
+   
+  )
+}
+
+export default App
---
+++ react01/src/oom.jsx
@@ -0,0 +1,6 @@
+function oom(){
+    return(
+        <h1>chai</h1>
+    )
+}
+export default oom;
